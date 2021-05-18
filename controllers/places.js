920749b317320c71bf10e16135e7145e808fe02c
amitmraval <amitmraval@yahoo.com>
2021-05-18 11:09:09 -0400

    added more routes

--- controllers/places.js
+++ controllers/places.js
@@ -26,6 +26,26 @@ router.get("/cafes", (req, res, next) => {
     .catch(next);
 });
 
+// Index: GET all the bars
+router.get("/bars", (req, res, next) => {
+  // 1. Get all of the places from the DB
+  Place.find({ types: "bar" })
+    // 2. Send them back to the client as JSON
+    .then((places) => res.json(places))
+    // 3. If there's an error pass it on!
+    .catch(next);
+});
+
+// Index: GET all the stores
+router.get("/stores", (req, res, next) => {
+  // 1. Get all of the places from the DB
+  Place.find({ types: "store" })
+    // 2. Send them back to the client as JSON
+    .then((places) => res.json(places))
+    // 3. If there's an error pass it on!
+    .catch(next);
+});
+
 // Index: GET all the places by name
 router.get("/:names", (req, res, next) => {
   // 1. Get all of the places from the DB
